83ba222d3b47e760351b35ce4c11c93096316abc
mmm141023 <925670706@qq.com>
2020-05-25 13:07:21 +0800

    add reset css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './style'
+import Globalstyle from './style'
 import App from './App';
 
+const Global = (
+    <div>
+        <Globalstyle/>
+        <App/>
+    </div>
+)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    Global,
+    document.getElementById('root')
 );
 
